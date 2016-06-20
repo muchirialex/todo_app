233f5190a9c5c0b3ccbea97497e57b239929da07
Alex Muchiri <muchiri94@gmail.com>
2016-06-20 20:27:50 +0300

    added mark as complete ability

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :todo_lists do
-  	resources :todo_items
+  	resources :todo_items do
+  		member do
+  			patch :complete
+  		end
+    end
   end
 
   root "todo_lists#index"
-
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620161450) do
+ActiveRecord::Schema.define(version: 20160620170148) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string   "content"
     t.integer  "todo_list_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.datetime "completed_at"
   end
 
   add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
